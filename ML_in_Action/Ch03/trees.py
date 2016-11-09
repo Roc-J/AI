aae1d035a9035762548b36472766f769b20a0d8e
Roc-J <646481338@qq.com>
2016-11-09 20:58:00 +0800

    add decision test

--- ML_in_Action/Ch03/trees.py
+++ ML_in_Action/Ch03/trees.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 from math import log
-
+import operator
 
 def calcShannonEnt(dataset):
     '''
@@ -44,6 +44,11 @@ def splitDataSet(dataSet, axis, value):
 
 
 def chooseBestFeatureToSplit(dataSet):
+    '''
+    选择最好的数据集划分方式
+    :param dataSet:
+    :return:
+    '''
     numFeatures = len(dataSet[0])-1
     baseEntropy = calcShannonEnt(dataSet)
     bestInfoGain = 0.0
@@ -61,3 +66,33 @@ def chooseBestFeatureToSplit(dataSet):
             bestInfoGain = infoGain
             bestFeature =i
     return bestFeature
+
+
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys():
+            classCount[vote]=0
+            classCount[vote] +=1
+    sortedClassCount = sorted(classCount.iteritems(),\
+                                  key=operator.itemgetter(1),reverse=True)
+    return sortedClassCount[0][0]
+
+def createTree(dataSet,labels):
+    '''创建树'''
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    if len(dataSet[0]) ==1:
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel:{}}
+    del(labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet,bestFeat,value),subLabels)
+    return myTree
+
